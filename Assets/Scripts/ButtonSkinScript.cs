5cc18fd022af279e1833e6c8c5bedf82338ece77
Arthur Brugiere <arbr1@hotmail.fr>
2017-07-23 21:11:48 +0200

    Select robot skin

---
+++ Assets/Scripts/ButtonSkinScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonSkinScript : MonoBehaviour {
+
+	public int skinNumber;
+	Button[] buttons;
+
+	void Start(){
+		GetComponent<Button> ().onClick.AddListener (TaskOnClick);
+		buttons = transform.parent.gameObject.GetComponentsInChildren<Button> ();
+	}
+
+	void TaskOnClick(){
+		PlayerPrefs.SetInt ("SkinNumber", skinNumber);
+
+
+		foreach (Button button in buttons) {
+			button.GetComponent<RectTransform> ().sizeDelta = new Vector2 (65, 65);
+		}
+			
+		GetComponent<RectTransform> ().sizeDelta = new Vector2 (75, 75);
+
+		Debug.Log ("[Saved] Skin Number : " + skinNumber);
+	}
+
+}
---
+++ Assets/Scripts/PlayerSkin.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerSkin : MonoBehaviour {
+
+	int skinNumber;
+
+	void Awake(){
+		skinNumber = PlayerPrefs.GetInt ("SkinNumber");
+		GetComponent<Animator> ().runtimeAnimatorController = Resources.Load ("RobotSkin" + skinNumber) as RuntimeAnimatorController;
+	}
+}
