ca055c27a68e8a24413202272b6c2dc6dddb56a1
Arthur Brugiere <arbr1@hotmail.fr>
2017-07-23 19:38:51 +0200

    Jump Smartphone

--- Assets/Scripts/Jump.cs
+++ Assets/Scripts/Jump.cs
@@ -57,14 +57,55 @@ public class Jump : MonoBehaviour {
         }
 
 		rb.velocity = new Vector2(10f, rb.velocity.y);
+
+		// Debug CornerGrab
+		if (rb.velocity.magnitude <= 0 && Time.timeSinceLevelLoad > 1) 
+			rb.AddForce (new Vector2 (0f, jumpForce));
     }
  
     void FixedUpdate()
     {
         //I placed this code in FixedUpdate because we are using phyics to move.
- 
-		Debug.Log( Input.GetAxis("Jump").GetTypeCode() );
 
+		#if ( UNITY_IPHONE || UNITY_ANDROID )
+		for (int i = 0; i < Input.touchCount; i++) {
+			//if you press down the mouse button...
+			if(Input.GetTouch(i).phase == TouchPhase.Began)
+			{
+				//and you are on the ground...
+				if(grounded || !doubleJump)
+				{
+					//jump!
+					rb.velocity = new Vector2 (rb.velocity.x, jumpForce);
+					stoppedJumping = false;
+
+					if (!grounded)
+						doubleJump = true;
+				}
+			}
+
+			//if you keep holding down the mouse button...
+			if((Input.GetTouch(0).phase == TouchPhase.Moved || Input.GetTouch(0).phase == TouchPhase.Stationary) && !stoppedJumping)
+			{
+				//and your counter hasn't reached zero...
+				if(jumpTimeCounter > 0)
+				{
+					//keep jumping!
+					rb.velocity = new Vector2 (rb.velocity.x, jumpForce);
+					jumpTimeCounter -= Time.deltaTime;
+				}
+			}
+
+
+			//if you stop holding down the mouse button...
+			if(Input.GetTouch(0).phase == TouchPhase.Ended || Input.GetTouch(0).phase == TouchPhase.Canceled)
+			{
+				//stop jumping and set your counter to zero.  The timer will reset once we touch the ground again in the update function.
+				jumpTimeCounter = jumpTime;
+				stoppedJumping = true;
+			}
+		}
+		#else
         //if you press down the mouse button...
 		if(Input.GetKeyDown(KeyCode.Space))
         {
@@ -100,5 +141,6 @@ public class Jump : MonoBehaviour {
 			jumpTimeCounter = jumpTime;
             stoppedJumping = true;
         }
+		#endif
     }
 }
